eb20d435ba23c1e13cb3440aaca944cf1f1af019
JeremyRand <biolizard89@gmail.com>
2019-03-05 16:21:01 +0000

    Add bits to checkpoint

--- electrum/blockchain.py
+++ electrum/blockchain.py
@@ -448,6 +448,8 @@ class Blockchain(util.PrintError):
             return 0
         if index == -1:
             return MAX_TARGET
+        if index + 1 == (constants.net.max_checkpoint() + 1) // 2016:
+            return self.bits_to_target(constants.net.VERIFICATION_BLOCK_LAST_BITS)
         # new target
         if index == constants.net.max_checkpoint() // 2016:
             first_timestamp = constants.net.VERIFICATION_BLOCK_FIRST_TIMESTAMP
