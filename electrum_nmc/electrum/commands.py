cfec6135a999b918af8180b94913063d4fcff48c
JeremyRand <jeremyrand@airmail.cc>
2020-04-05 21:53:13 +0000

    Namecoin: Improve "not found" error in name_show
    Thanks to Forest Johnson for reporting this UX bug at:
    https://www.sequentialread.com/how-to-register-a-namecoin-bit-domain-with-electrum-nmc/

--- electrum_nmc/electrum/commands.py
+++ electrum_nmc/electrum/commands.py
@@ -1156,9 +1156,12 @@ class Commands:
         unmined_height = max_chain_height - 18
 
         tx_best = None
+        expired_tx_exists = False
+        unmined_tx_exists = False
         for tx_candidate in txs[::-1]:
             if tx_candidate["height"] < unexpired_height:
                 # Transaction is expired.  Skip.
+                expired_tx_exists = True
                 continue
             if tx_candidate["height"] > unverified_height:
                 # Transaction doesn't have enough verified depth.  What we do
@@ -1167,6 +1170,7 @@ class Commands:
 
                 if tx_candidate["height"] > unmined_height:
                     # Transaction is new; skip in favor of an older one.
+                    unmined_tx_exists = True
                     continue
 
                 # We can't verify the transaction because we're still syncing,
@@ -1177,8 +1181,12 @@ class Commands:
             tx_best = tx_candidate
             break
 
+        if unmined_tx_exists:
+            raise NameNotFoundError("Name is purportedly unconfirmed")
+        if expired_tx_exists:
+            raise NameNotFoundError("Name is purportedly expired")
         if tx_best is None:
-            raise NameNotFoundError("Name never existed, is expired, or is unconfirmed")
+            raise NameNotFoundError("Name purportedly never existed")
         txid = tx_best["tx_hash"]
         height = tx_best["height"]
 
