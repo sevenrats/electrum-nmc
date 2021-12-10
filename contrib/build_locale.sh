6a3c875553269ee4dba4774fca4e4d7a016eaa7b
yanmaani <yanmaani@cock.li>
2021-12-10 12:00:00 +0000

    build: refactor out locale generation into build_locale.sh
    From Electrum PR 7594

---
+++ contrib/build_locale.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ! which msgfmt > /dev/null 2>&1; then
+    echo "Please install gettext"
+    exit 1
+fi
+
+for i in "$1/"*; do
+  mkdir -p "$i/LC_MESSAGES"
+  (msgfmt --output-file="$i/LC_MESSAGES/electrum.mo" "$i/electrum.po" || true)
+done
