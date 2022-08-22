spesmilo/electrum

win build: set `--no-build-isolation` when pip installing pyinstaller
fixes https://github.com/spesmilo/electrum/issues/7736

contrib/build-wine/prepare-wine.sh
@@ -105,6 +105,6 @@ info "Building PyInstaller."
     [[ -e "PyInstaller/bootloader/Windows-$PYINST_ARCH/runw.exe" ]] || fail "Could not find runw.exe in target dir!"
 ) || fail "PyInstaller build failed"
 info "Installing PyInstaller."
-$WINE_PYTHON -m pip install --no-dependencies --no-warn-script-location ./pyinstaller
+$WINE_PYTHON -m pip install --no-dependencies --no-build-isolation --no-warn-script-location ./pyinstaller
 
 info "Wine is configured."

sevenrats/electrum-nmc

Backported from Electrum v4.2.1.

contrib/build-wine/prepare-wine.sh
@@ -112,6 +112,6 @@ info "Building PyInstaller."
     fi
 ) || fail "PyInstaller build failed"
 info "Installing PyInstaller."
-$PYTHON -m pip install --no-dependencies --no-warn-script-location ./pyinstaller
+$PYTHON -m pip install --no-dependencies --no-build-isolation --no-warn-script-location ./pyinstaller
 
 info "Wine is configured."